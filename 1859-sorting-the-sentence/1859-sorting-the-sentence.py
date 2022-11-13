31dd7a47802c9c6c90fa274267d07ddda1debfdb
Ramprasad <83056522+Ramprasad-muppana@users.noreply.github.com>
2022-11-13 10:18:26 +0530

    Time: 62 ms (23.18%), Space: 14 MB (11.40%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        a = s[::-1].split()
+        a.sort()
+        r = []  
+        for word in a:
+            r.append(word[1:][::-1])
+        return " ".join(r)
