366b94e32a97f6f37ce308224f709f2985a270ae
Ramprasad <83056522+Ramprasad-muppana@users.noreply.github.com>
2023-01-30 21:31:27 +0530

    Added solution - LeetHub

---
+++ The Bit Game - GFG/the-bit-game.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int swapBitGame(long long n)
+    {
+        int c=0;
+        while(n)
+        {
+            n=n&(n-1);
+            c+=1;
+        }
+        return 2 - (c%2);
+        // code here 
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.swapBitGame(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
